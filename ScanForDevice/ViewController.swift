d1f7ce14f1abf7bf051b7e07832516cff5cde703
daire mc daid <dairemcdaid@daires-macbook-air.local>
2017-12-17 22:01:16 +0000

    Initial Commit

---
+++ ScanForDevice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScanForDevice
+//
+//  Created by daire mc daid on 17/12/2017.
+//  Copyright © 2017 daire mc daid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
